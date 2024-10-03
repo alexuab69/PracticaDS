30f4359057a83a41f145af05f21295e8374f4813
Adria-Lopez789 <adrialopezuab@gmail.com>
2024-10-03 13:49:22 +0200

    prueba de cambio2

--- src/baseNoStates/Actions.java
+++ src/baseNoStates/Actions.java
@@ -8,5 +8,4 @@ public final class Actions {
   // possible actions in door requests
   public static final String OPEN = "open";
   public static final String CLOSE = "close";
-  public static final String YIPIE = "close";
 }
