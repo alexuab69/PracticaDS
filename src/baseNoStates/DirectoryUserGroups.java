e58fd736c3bf85a030234a12225b04656ad87661
alexuab69 <1634223@uab.cat>
2024-10-12 00:23:30 +0200

    Rama Alex Uml Users

---
+++ src/baseNoStates/DirectoryUserGroups.java
@@ -0,0 +1,16 @@
+package baseNoStates;
+
+import java.util.ArrayList;
+
+public final class DirectoryUserGroups {
+    private static ArrayList<UserGroup> userGroups = new ArrayList<>();
+
+    public void makeUserGroups() {
+
+    }
+    public static User findUserByCredential(String credential) {
+        for (UserGroup userGroup : userGroups) {
+
+        }
+    }
+}
---
+++ src/baseNoStates/UserGroup.java
@@ -0,0 +1,7 @@
+package baseNoStates;
+
+import java.util.ArrayList;
+
+public class UserGroup {
+    private ArrayList<User> users = new ArrayList<>();
+}
