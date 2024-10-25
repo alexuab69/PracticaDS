5ce5102d50db1cd7b235008bab13643024511fbd
Adrià López Gaya <adrialopezuab@gmail.com>
2024-10-25 19:56:51 +0200

    Update UserGroupEmployee.java

--- src/baseNoStates/userGroups/UserGroupEmployee.java
+++ src/baseNoStates/userGroups/UserGroupEmployee.java
@@ -2,31 +2,41 @@ package baseNoStates.userGroups;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.EnumSet;
+import java.time.LocalTime;
+
+import java.util.ArrayList;
 
 import baseNoStates.partitions.Area;
 
 public class UserGroupEmployee extends UserGroup{
 
   public UserGroupEmployee(String role) {
-
     super(role);
 
-    // Set default values for Employee
-    // Sept 1 2024 to Mar 1 2025
-    // week days 9-17h
-    LocalDateTime firstDate = LocalDateTime.of(LocalDateTime.now().getYear(), 9, 1, 0, 0);
-    LocalDateTime finalDate = LocalDateTime.of(LocalDateTime.now().plusYears(1).getYear(), 3, 1, 0, 0);
-    setDateRange(firstDate, finalDate);
-    setWorkingDays(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
-        DayOfWeek.THURSDAY, DayOfWeek.FRIDAY));
-    setWorkingHours(9, 17);  // 9:00 AM - 5:00 PM
+    ArrayList<DayOfWeek> workingDays = new ArrayList<>();
+    workingDays.add(DayOfWeek.MONDAY);
+    workingDays.add(DayOfWeek.TUESDAY);
+    workingDays.add(DayOfWeek.WEDNESDAY);
+    workingDays.add(DayOfWeek.THURSDAY);
+    workingDays.add(DayOfWeek.FRIDAY);
+
+    // Step 2: Define the working hours
+    LocalTime startWorkingHour = LocalTime.of(9, 0);  // 08:00
+    LocalTime endWorkingHour = LocalTime.of(17, 0);    // 20:00
+
+    // Step 3: Define the start and end dates
+    LocalDateTime startDate = LocalDateTime.of(2024, 9, 1, 0, 0);  // September 1, 2024
+    LocalDateTime endDate = LocalDateTime.of(2025, 3, 1, 0, 0);    // March 1, 2025
+
+    // Step 4: Create the Schedule instance
+    schedule = new Schedule(workingDays, startWorkingHour, endWorkingHour, startDate, endDate);
   }
 
   @Override
+  public boolean canSendRequests(LocalDateTime now) { return schedule.inRange(now);}
+
+    @Override
   public boolean canBeInSpaceAndDoAction(Area toSpace, String action) {
-    // just shortly unlock
-    // ground floor, floor1, exterior, stairs (this, for all), that is, everywhere but the parking
     return (action.equals("unlock_shortly") && toSpace.getId().equals("parking"))
         || action.equals("open")
         || action.equals("close");
